6e02665a7db0606dd0b257476cb2711ea04110b3
eddiec68 <eddiec68@nycstudents.net>
2026-02-12 10:13:42 -0500

    Update me.py

---
+++ me.py
@@ -0,0 +1,27 @@
+""" count = 1
+
+while count <= 5:
+    print("This is loop number", count)
+    count = count + 1 """
+
+""" order = ""
+
+while order != "done":
+    order = input("What would you like to order? (type 'done' to finish): ")
+
+print("Thanks for your order!") """
+
+""" count = 10
+while count >= 1:
+    print("this is number", count)
+    count = count - 1 """
+""" 
+color =""
+while color != "stop":
+    color = input("What is your favorite color? (type stop to finish)")
+print("Cool Color!") """
+
+
+RandomNumber = int(str("1,2,3,4,5,6,7,8,9,10"))
+print 
+
